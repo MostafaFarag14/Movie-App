fbf1cb8f88bafba9d5fd10a76c9aa0affea58639
mostafafarag <mostafafarag140@gmail.com>
2020-03-14 02:20:13 +0200

    Updates

---
+++ precache-manifest.88bba39fcec3c3f64cdedde0c5b0191b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f277fcae75b43d44dbcc4cd82e3dacd2",
+    "url": "/Movie-App/index.html"
+  },
+  {
+    "revision": "c51118c35cd9f8278568",
+    "url": "/Movie-App/static/css/main.5a38d4ea.chunk.css"
+  },
+  {
+    "revision": "840d914ef2b86c4a9418",
+    "url": "/Movie-App/static/js/2.1c88cbdf.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Movie-App/static/js/2.1c88cbdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c51118c35cd9f8278568",
+    "url": "/Movie-App/static/js/main.db67ad0c.chunk.js"
+  },
+  {
+    "revision": "7568e5daa7a4a0d89128",
+    "url": "/Movie-App/static/js/runtime-main.8e34a323.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Movie-App/static/media/logo.5d5d9eef.svg"
+  }
+]);
